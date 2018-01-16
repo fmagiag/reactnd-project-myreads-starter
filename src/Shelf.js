f9781b8319aef5a6c9d88df08461511dad3d59d2
Fernando Magiag <fernando.magiag@lalubema.com>
2018-01-16 20:12:26 -0200

    FIX Stateless Functional Component

--- src/Shelf.js
+++ src/Shelf.js
@@ -1,27 +1,24 @@
-import React, {Component} from 'react'
+import React from 'react'
 import Book from './Book'
 
-class Shelf extends Component{
-  render(){
-    const {books, name, onChangeBook} = this.props
-    return(
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map(book => (
-              <li key={book.id} >
-                <Book
-                  book={book}
-                  onMoved={onChangeBook}
-                />
-              </li>
-            ))}
-          </ol>
-        </div>
+const Shelf = (props) => {
+  return(
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.name}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map(book => (
+            <li key={book.id} >
+              <Book
+                book={book}
+                onMoved={props.onChangeBook}
+              />
+            </li>
+          ))}
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Shelf
